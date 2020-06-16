b393be9a3dd1de57fcd90369fa90e6fbda4edcee
Sajid Anjum <sajid1760@gmail.com>
2020-06-16 05:08:35 +0000

    Automatically backed up by Learn

--- spec/04_associations_spec.rb
+++ spec/04_associations_spec.rb
@@ -23,7 +23,7 @@ describe "Associations" do
       end
     end
 
-  
+  do Owner.reset_all end
 
     describe "#dogs" do
       it "returnsa a collection of all the dogs that belong to the owner" do
