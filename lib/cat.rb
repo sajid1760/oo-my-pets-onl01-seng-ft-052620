b662346fcf08146676f00ad22036ed29990e3368
Sajid Anjum <sajid1760@gmail.com>
2020-06-16 04:40:04 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,20 @@
 class Cat
-  # code goes here
+  
+attr_reader :name, :species
+attr_accessor :owner, :mood
+
+@@all = []
+  
+  def initialize(name,owner)
+    @name = name
+    self.owner = owner
+    @mood = "nervous"
+    @species = "cat"
+    @@all << self
+  end
+
+  def Cat.all
+    @@all
+  end
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,49 @@
 class Owner
-  # code goes here
+  
+attr_reader :name, :species
+
+@@all = []
+  
+  def initialize(name = "unknown")
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all = []
+  end
+  
+  def cats 
+    catvector = []
+    Cat.all.each  do |cat| 
+      if cat.owner.name == self.name
+        then catvector << cat
+      end
+    end
+    catvector
+  end
+  
+  def dogs 
+    dogvector = []
+    Dog.all.each  do |dog| 
+      if dog.owner.name == self.name
+        then dogvector << dog
+      end
+    end
+    dogvector
+  end
+  
+  def say_species
+    "I am a human."
+  end
+
 end
